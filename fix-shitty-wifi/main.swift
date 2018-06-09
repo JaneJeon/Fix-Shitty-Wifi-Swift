52d309cee2dd10700e27c8661f9dc0d963331aaf
Jane Jeon <sungil.ahn.19@dartmouth.edu>
2018-06-09 13:39:15 +0900

    Initial Commit

---
+++ fix-shitty-wifi/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  fix-shitty-wifi
+//
+//  Created by Jane Jeon on 6/9/18.
+//  Copyright © 2018 Jane Jeon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
